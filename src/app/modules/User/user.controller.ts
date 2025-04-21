95290628db608b3688acb46743346b63c02119b0
sowmik-sec <sowmik.sec@gmail.com>
2025-04-21 18:18:57 +0600

    Response Formatting and Exception Handling

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -2,9 +2,20 @@ import { Request, Response } from "express";
 import { UserService } from "./user.service";
 
 const createAdmin = async (req: Request, res: Response) => {
-  //   console.log(req.body);
-  const result = await UserService.createAdmin(req.body);
-  res.send(result);
+  try {
+    //   console.log(req.body);
+    const result = await UserService.createAdmin(req.body);
+    res.status(200).json({
+      success: true,
+      message: "Admin created successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error?.name || "Something went wrong",
+    });
+  }
 };
 
 export const UserController = {
