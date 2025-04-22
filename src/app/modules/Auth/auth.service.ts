0bcad14b9ec987d59ff5520ead007c48244baf3c
sowmik-sec <sowmik.sec@gmail.com>
2025-04-22 20:18:54 +0600

    Generating Access Tokens for User Login

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,5 +1,25 @@
+import prisma from "../../../shared/prisma";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const loginUser = async (payload: { email: string; password: string }) => {
-  console.log(payload);
+  const userData = await prisma.user.findUniqueOrThrow({
+    where: {
+      email: payload.email,
+    },
+  });
+  const isCorrectPassword = await bcrypt.compare(
+    payload.password,
+    userData.password
+  );
+  const accessToken = jwt.sign(
+    {
+      email: userData.email,
+      role: userData.role,
+    },
+    "abcdefg",
+    { algorithm: "HS256", expiresIn: "15m" }
+  );
+  console.log(accessToken);
 };
 
 export const AuthServices = {
