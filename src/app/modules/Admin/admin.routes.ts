2cbca5bc4aff1e24a12679c4b2ee40f63e442ca6
sowmik-sec <sowmik.sec@gmail.com>
2025-04-23 12:13:59 +0600

    Implementing Role-Based Access Control on API Routes

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -4,15 +4,34 @@ const router = express.Router();
 import { AnyZodObject, z } from "zod";
 import validateRequest from "../../middlewares/validateRequest";
 import { AdminValidations } from "./admin.validations";
+import auth from "../../middlewares/auth";
+import { UserRole } from "../../../generated/prisma";
 
-router.get("/", AdminControllers.getAllAdmins);
-router.get("/:id", AdminControllers.getAdminById);
+router.get(
+  "/",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminControllers.getAllAdmins
+);
+router.get(
+  "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminControllers.getAdminById
+);
 router.patch(
   "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
   validateRequest(AdminValidations.updateAdminZodSchema),
   AdminControllers.updateAdmin
 );
-router.delete("/:id", AdminControllers.deleteAdmin);
-router.delete("/soft/:id", AdminControllers.softDeleteAdmin);
+router.delete(
+  "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminControllers.deleteAdmin
+);
+router.delete(
+  "/soft/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AdminControllers.softDeleteAdmin
+);
 
 export const AdminRoutes = router;
