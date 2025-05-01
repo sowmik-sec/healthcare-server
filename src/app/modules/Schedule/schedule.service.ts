1c95dbed06096a3f7c96a924f67e192df8c023c0
sowmik-sec <sowmik.sec@gmail.com>
2025-05-01 10:58:18 +0600

    Creating Schedule: Part 2

--- src/app/modules/Schedule/schedule.service.ts
+++ src/app/modules/Schedule/schedule.service.ts
@@ -1,4 +1,25 @@
-const insertIntoDB = async (payload: any) => {};
+import { addHours, format } from "date-fns";
+
+const insertIntoDB = async (payload: any) => {
+  const { startDate, endDate, startTime, endTime } = payload;
+  const currentDate = new Date(startDate);
+  const lastDate = new Date(endDate);
+  while (currentDate <= endDate) {
+    const startDatetime = new Date(
+      addHours(
+        `${format(currentDate, "yyyy-MM-dd")}`,
+        Number(startTime.split(":")[0])
+      )
+    );
+    const endDatetime = new Date(
+      addHours(
+        `${format(lastDate, "yyyy-MM-dd")}`,
+        Number(endTime.split(":")[0])
+      )
+    );
+    while (startDatetime <= endDatetime) {}
+  }
+};
 
 export const ScheduleServices = {
   insertIntoDB,
