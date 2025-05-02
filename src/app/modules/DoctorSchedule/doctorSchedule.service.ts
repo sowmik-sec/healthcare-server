ae19f576928a38600c0db6cb0afa58baccf35a2b
sowmik-sec <sowmik.sec@gmail.com>
2025-05-02 14:56:03 +0600

    Creating Doctor Schedule: Part 2

--- src/app/modules/DoctorSchedule/doctorSchedule.service.ts
+++ src/app/modules/DoctorSchedule/doctorSchedule.service.ts
@@ -7,12 +7,19 @@ const insertIntoDB = async (
     scheduleIds: string[];
   }
 ) => {
-  const doctorData = await prisma.doctor.findUnique({
+  const doctorData = await prisma.doctor.findUniqueOrThrow({
     where: {
       email: user?.email,
     },
   });
-  console.log(payload.scheduleIds);
+  const doctorScheduleData = payload.scheduleIds.map((scheduleId) => ({
+    doctorId: doctorData.id,
+    scheduleId,
+  }));
+  const result = await prisma.doctorSchedules.createMany({
+    data: doctorScheduleData,
+  });
+  return result;
 };
 
 export const DoctorScheduleServices = {
