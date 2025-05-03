fc4583db987c78a9aae092daec8e66d7e71c5e54
sowmik-sec <sowmik.sec@gmail.com>
2025-05-03 21:17:59 +0600

    Creating Prescription: Part 2

--- src/app/modules/Prescription/prescription.service.ts
+++ src/app/modules/Prescription/prescription.service.ts
@@ -1,15 +1,44 @@
-import { AppointmentStatus, PaymentStatus } from "../../../generated/prisma";
+import { StatusCodes } from "http-status-codes";
+import {
+  AppointmentStatus,
+  PaymentStatus,
+  Prescription,
+} from "../../../generated/prisma";
 import prisma from "../../../shared/prisma";
+import ApiError from "../../errors/ApiError";
 import { TAuthUser } from "../../interfaces/common";
 
-const insertIntoDB = async (user: TAuthUser, payload: any) => {
+const insertIntoDB = async (
+  user: TAuthUser,
+  payload: Partial<Prescription>
+) => {
+  console.log(payload);
   const appointmentData = await prisma.appointment.findUniqueOrThrow({
     where: {
       id: payload.appointmentId,
       status: AppointmentStatus.COMPLETED,
       paymentStatus: PaymentStatus.PAID,
     },
+    include: {
+      doctor: true,
+    },
   });
+  if (!(user?.email === appointmentData.doctor.email)) {
+    throw new ApiError(StatusCodes.BAD_REQUEST, "This is not your appointment");
+  }
+  const result = await prisma.prescription.create({
+    data: {
+      appointmentId: appointmentData.id,
+      doctorId: appointmentData.doctorId,
+      patientId: appointmentData.patientId,
+      instructions: payload.instructions as string,
+      followUpDate: payload.followUpDate || null || undefined,
+    },
+    include: {
+      patient: true,
+    },
+  });
+  return result;
 };
 
 export const PrescriptionServices = {
