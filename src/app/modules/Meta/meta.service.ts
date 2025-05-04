1ed40c448fa176785041f3181a0fa18a662edd41
sowmik-sec <sowmik.sec@gmail.com>
2025-05-04 18:10:00 +0600

    Retrieving Dashboard Metadata: Part 2

--- src/app/modules/Meta/meta.service.ts
+++ src/app/modules/Meta/meta.service.ts
@@ -2,6 +2,7 @@ import { StatusCodes } from "http-status-codes";
 import { UserRole } from "../../../generated/prisma";
 import ApiError from "../../errors/ApiError";
 import { TAuthUser } from "../../interfaces/common";
+import prisma from "../../../shared/prisma";
 
 const fetchDAshboardMetaData = async (user: TAuthUser) => {
   switch (user?.role) {
@@ -12,7 +13,7 @@ const fetchDAshboardMetaData = async (user: TAuthUser) => {
       getAdminMetaData();
       break;
     case UserRole.DOCTOR:
-      getDoctorMetaData();
+      getDoctorMetaData(user as TAuthUser);
       break;
     case UserRole.PATIENT:
       getPatientMetaData();
@@ -22,9 +23,31 @@ const fetchDAshboardMetaData = async (user: TAuthUser) => {
   }
 };
 
-const getAdminMetaData = async () => {};
 const getSuperAdminMetaData = async () => {};
-const getDoctorMetaData = async () => {};
+const getAdminMetaData = async () => {
+  const appointmentCount = await prisma.appointment.count();
+  const patientCount = await prisma.patient.count();
+  const doctorCount = await prisma.doctor.count();
+  const paymentCount = await prisma.payment.count();
+  const totalRevenue = await prisma.payment.aggregate({
+    _sum: { amount: true },
+  });
+};
+const getDoctorMetaData = async (user: TAuthUser) => {
+  const doctorData = await prisma.doctor.findUniqueOrThrow({
+    where: {
+      email: user?.email,
+    },
+  });
+  const appointmentCount = await prisma.appointment.count({
+    where: {
+      doctorId: doctorData.id,
+    },
+  });
+  const patientCount = await prisma.appointment.groupBy({
+    by: ["patientId"],
+  });
+};
 const getPatientMetaData = async () => {};
 
 export const MetaServices = {
