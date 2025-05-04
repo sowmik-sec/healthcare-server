7ebb6236fec6b7b133db12c46529d54214397c3f
sowmik-sec <sowmik.sec@gmail.com>
2025-05-04 19:29:55 +0600

    Seeding Super Admin Data into the Database

---
+++ prisma/seed.ts
@@ -0,0 +1,36 @@
+import { UserRole } from "../src/generated/prisma";
+import prisma from "../src/shared/prisma";
+
+const seedSuperAdmin = async () => {
+  try {
+    const isSuperAdminExists = await prisma.user.findFirst({
+      where: {
+        role: UserRole.SUPER_ADMIN,
+      },
+    });
+    if (isSuperAdminExists) {
+      console.log("Super admin already exists");
+      return;
+    }
+    const superAdminData = await prisma.user.create({
+      data: {
+        email: "super@admin.com",
+        password: "1234",
+        role: "SUPER_ADMIN",
+        admin: {
+          create: {
+            name: "Super Admin",
+            contactNumber: "123456789",
+          },
+        },
+      },
+    });
+    console.log("Super admin created successfully", superAdminData);
+  } catch (error) {
+    console.error("Error");
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+seedSuperAdmin();
