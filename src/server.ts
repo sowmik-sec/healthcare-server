1339948b8b053786591717e55b275d04c8b0982f
sowmik-sec <sowmik.sec@gmail.com>
2025-05-04 20:03:36 +0600

    Handling uncaughtException and unhandledRejection Errors

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,23 @@ async function main() {
   const server: Server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
+  const exitHandler = () => {
+    if (server) {
+      server.close(() => {
+        console.info("Server closed!");
+      });
+    }
+    process.exit(1);
+  };
+
+  process.on("uncaughtException", (error) => {
+    console.log(error);
+    exitHandler();
+  });
+  process.on("unhandledRejection", (error) => {
+    console.log(error);
+    exitHandler();
+  });
 }
 
 main();
